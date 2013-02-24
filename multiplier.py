179c4565fd0121bad7903c53acad5265217b860d
beepscore <support@beepscore.com>
2013-02-24 13:02:07 -0800

    Capitalize class name Multiplier per Python convention.
    Reference:
    http://www.python.org/dev/peps/pep-0008/#class-names

--- multiplier.py
+++ multiplier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-class multiplier():
+class Multiplier():
     '''
     Multiply two numbers
     '''
--- test-multiplier.py
+++ test-multiplier.py
@@ -11,15 +11,15 @@ class TestMultiplier(unittest.TestCase):
         a = 0
         b = 0
         expected_result = 0
-        # module multiplier, class multiplier, method multiply(a, b)
-        result = multiplier.multiplier.multiply(a, b)
+        # module multiplier, class Multiplier, method multiply(a, b)
+        result = multiplier.Multiplier.multiply(a, b)
         self.assertEqual(expected_result, result,
                          'multiply({}, {}) expected {} but got {}'.format(a, b, expected_result, result))
 
         a = 1
         b = 1
         expected_result = 1
-        result = multiplier.multiplier.multiply(a, b)
+        result = multiplier.Multiplier.multiply(a, b)
         self.assertEqual(expected_result, result,
                          'multiply({}, {}) expected {} but got {}'.format(a, b, expected_result, result))
 
@@ -27,8 +27,8 @@ class TestMultiplier(unittest.TestCase):
         a = 7
         b = 9
         expected_result = 63
-        # module multiplier, class multiplier, method multiply_iterative(a, b)
-        result = multiplier.multiplier.multiply_iterative(a, b)
+        # module multiplier, class Multiplier, method multiply_iterative(a, b)
+        result = multiplier.Multiplier.multiply_iterative(a, b)
         self.assertEqual(expected_result, result,
                          'multiply_iterative({}, {}) expected {} but got {}'.format(a, b, expected_result, result))
 
