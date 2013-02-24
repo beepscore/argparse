13ff0953d702c68da3e9a0bc13433b8f38e956f4
beepscore <support@beepscore.com>
2013-02-24 11:45:17 -0800

    Add function multiply_iterative(a, b)
    Reference:
    http://stackoverflow.com/questions/14638078/russian-peasant-multiplication-python-3-3?rq=1

--- multiplier.py
+++ multiplier.py
@@ -33,10 +33,36 @@ def main():
     arguments = parser.parse_args()
 
     multiply_result = multiply(arguments.multiplicand, arguments.multiplier)
-    print(arguments.multiplicand, 'times', arguments.multiplier, 'equals', multiply_result)
+    print('multiply({}, {}) = {}'.format(arguments.multiplicand, arguments.multiplier, multiply_result))
+
+    multiply_iterative_result = multiply_iterative(arguments.multiplicand, arguments.multiplier)
+    print('multiply_iterative({}, {}) = {}'.format(arguments.multiplicand, arguments.multiplier, multiply_iterative_result))
+
 
 def multiply(a, b):
     return (a * b)
+
+def multiply_iterative(a, b):
+    '''
+    http://stackoverflow.com/questions/14638078/russian-peasant-multiplication-python-3-3?rq=1
+    '''
+
+    result = 0
+    multiplicand_iterative = a
+    multiplier_iterative = b
+
+    while multiplier_iterative > 1:
+        if ((multiplier_iterative % 2) == 1):
+            result += multiplicand_iterative
+        multiplicand_iterative *= 2
+        # use integer division //
+        multiplier_iterative //= 2
+        #print('multiplicand_iterative:{} multiplier_iterative:{} result:{}'.format(multiplicand_iterative, multiplier_iterative, result))
+
+    if (multiplier_iterative == 1):
+        result += multiplicand_iterative
+
+    return result
 
 if __name__ == "__main__": main()
 
